ceb087819b2ac68ae79e77a29e7ec41c580801d3
Kendy <Benoldk@gmail.com>
2021-09-16 02:06:46 -0400

    stumble feature implementation

---
+++ Assets/scripts/fsm/actions/StumbleAction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace game.package.fsm
+{
+    [CreateAssetMenu(menuName = "FSM/Actions/StumbleAction", fileName = "Stumble-action")]
+    public class StumbleAction : Action
+    {
+        public override void Act(StateController controller)
+        {
+            Stumble(controller);
+            UpdatePosition(controller);
+        }
+
+        private void Stumble(StateController controller)
+        {
+            controller.animator.SetBool("isStumbling", controller.isStumbling);
+        }
+    }
+}
